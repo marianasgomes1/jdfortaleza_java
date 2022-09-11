b4a13e543779c3771d0aacfd750d5718f97c918e
marianasgomes1 <112919369+marianasgomes1@users.noreply.github.com>
2022-09-11 12:06:59 -0300

    Add files via upload

---
+++ JDJAVA.java
@@ -0,0 +1,26 @@
+package jd1;
+
+import java.util.Scanner;
+
+public class JDJAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print ("Bom dia");
+		
+Scanner leitor = new Scanner(System.in) {
+
+	String variavel;
+	
+	System.out.print ("Qual seu nome?")  
+	variavel = leitor.nextLine();
+	 
+	String idade;
+	idade=leitor.nextLine();
+
+
+	System.out.println("Ola "+ variavel + "sua idade é: " + idade");"
+}
+	}
+	}
